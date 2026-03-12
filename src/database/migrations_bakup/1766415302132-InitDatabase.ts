3705cd4f21ce58342ed334d63d4b32dc46de92a5
yeonoel <yeopevroguinoel@gmail.com>
2026-03-12 20:38:47 +0000

    chore: config prod Railway + Neon

--- src/database/migrations_bakup/1766415302132-InitDatabase.ts
+++ src/database/migrations_bakup/1766415302132-InitDatabase.ts
@@ -4,6 +4,9 @@ export class InitDatabase1766415302132 implements MigrationInterface {
     name = 'InitDatabase1766415302132'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // ENUMS
+        await queryRunner.query(`CREATE TYPE "public"."addresses_address_type_enum" AS ENUM('shipping', 'billing')`);
+
         await queryRunner.query(`CREATE TABLE "addresses" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "address_type" "public"."addresses_address_type_enum" NOT NULL DEFAULT 'shipping', "is_default" boolean NOT NULL DEFAULT false, "street_address" character varying(255) NOT NULL, "apartment" character varying(100), "city" character varying(100) NOT NULL, "state" character varying(100), "postal_code" character varying(20) NOT NULL, "country" character varying(100) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_745d8f43d3af10ab8247465e450" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "idx_addresses_user_id" ON "addresses" ("user_id") `);
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying(255) NOT NULL, "password" character varying(255) NOT NULL, "first_name" character varying(100) NOT NULL, "last_name" character varying(100) NOT NULL, "phone" character varying(20), "avatar_url" character varying(500), "email_verified" boolean NOT NULL DEFAULT false, "is_active" boolean NOT NULL DEFAULT true, "role" "public"."users_role_enum" NOT NULL DEFAULT 'customer', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "last_login_at" TIMESTAMP, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
