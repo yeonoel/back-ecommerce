c112d32268c7b58a8c70c5fcd87dff061e9a99d5
yeonoel <yeopevroguinoel@gmail.com>
2026-03-13 17:02:39 +0000

    chore: clean migration for prod

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,13 +4,15 @@ import { PaymentsController } from './payments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { Order } from '../orders/entities/order.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Payment, Order]),
+    ConfigModule
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService]
 })
-export class PaymentsModule {}
+export class PaymentsModule { }
